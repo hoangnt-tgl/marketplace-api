3981ffd321f3fc2d9cff16aca4ffe016186fff8b
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-01-14 14:58:51 +0700

    create his

--- src/controllers/order.controllers.ts
+++ src/controllers/order.controllers.ts
@@ -12,6 +12,7 @@ import {
 	createService,
 	queryExistService,
 	findManyService,
+	deleteOneService,
 } from "../services/model.services";
 import userModel from "../models/user.model";
 
@@ -25,70 +26,64 @@ const sellItem = async (req: Request, res: Response) => {
 	try {
 		let { userAddress, chainId } = req.params;
 		userAddress = userAddress.toLowerCase();
-		let {price, quantity, itemId, to, txHash, collectionId, owner, collectionName, itemName} = req.body;
-        let collectionInfo = await findOneService(collectionModel, {collectionName, userAddress: owner, chainId});
-        if(!collectionInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
-        let itemInfo = await findOneService(itemModel, {itemName, collectionId: collectionInfo._id});
-        if(!itemInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
-        let newOrder = {
-            maker: userAddress,
-            chainId: chainId,
-            quantity: quantity,
-            itemId: itemInfo._id,
-            basePrice: price,
-            type: 6,
-        }
+		let { price, quantity, itemId, to, txHash, collectionId, owner, collectionName, itemName } = req.body;
+		let collectionInfo = await findOneService(collectionModel, { collectionName, userAddress: owner, chainId });
+		if (!collectionInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
+		let itemInfo = await findOneService(itemModel, { itemName, collectionId: collectionInfo._id });
+		if (!itemInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
+		let newOrder = {
+			maker: userAddress,
+			chainId: chainId,
+			quantity: quantity,
+			itemId: itemInfo._id,
+			basePrice: price,
+			type: 6,
+		};
 		let orderInfo = await createService(orderModel, newOrder);
 		let newHistory = {
 			collectionId: collectionInfo._id,
-            itemId: itemInfo._id,
+			itemId: itemInfo._id,
 			from: userAddress,
 			to: to,
-            quantity: quantity,
+			quantity: quantity,
 			type: 6,
 			txHash: txHash,
-            price: price,
+			price: price,
 		};
 		createService(historyModel, newHistory);
 		return res.status(200).json({ data: orderInfo });
 	} catch (error: any) {
+		console.log(error);
 		return res.status(500).json({ error: ERROR_RESPONSE[500] });
 	}
 };
 
 const buyItem = async (req: Request, res: Response) => {
-    try {
-        let { userAddress, chainId } = req.params;
-        userAddress = userAddress.toLowerCase();
-        let {quantity, itemId, to, txHash, collectionId, owner, collectionName, itemName, price} = req.body;
-        let collectionInfo = await findOneService(collectionModel, {collectionName, userAddress: owner, chainId});
-        if(!collectionInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
-        let itemInfo = await findOneService(itemModel, {itemName, collectionId: collectionInfo._id});
-        if(!itemInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
-        let newOrder = {
-            maker: userAddress,
-            chainId: chainId,
-            quantity: quantity,
-            itemId: itemInfo._id,
-            basePrice: price,
-            type: 7,
-        }
-        let orderInfo = await createService(orderModel, newOrder);
-        let newHistory = {
-            collectionId: collectionInfo._id,
-            itemId: itemInfo._id,
-            from: userAddress,
-            to: to,
-            quantity: quantity,
-            type: 7,
-            txHash: txHash,
-            price: price,
-        };
-        createService(historyModel, newHistory);
-        return res.status(200).json({ data: orderInfo });
-    } catch (error: any) {
-        return res.status(500).json({ error: ERROR_RESPONSE[500] });
-    }
+	try {
+		let { userAddress, chainId } = req.params;
+		userAddress = userAddress.toLowerCase();
+		let { quantity, itemId, to, txHash, collectionId, owner, collectionName, itemName, price } = req.body;
+		let collectionInfo = await findOneService(collectionModel, { collectionName, userAddress: owner, chainId });
+		if (!collectionInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
+		let itemInfo = await findOneService(itemModel, { itemName, collectionId: collectionInfo._id });
+		if (!itemInfo) return res.status(404).json({ error: ERROR_RESPONSE[404] });
+		deleteOneService(orderModel, { itemId: itemInfo._id });
+		let newHistory = {
+			collectionId: collectionInfo._id,
+			itemId: itemInfo._id,
+			from: userAddress,
+			to: to,
+			quantity: quantity,
+			type: 7,
+			txHash: txHash,
+			price: price,
+		};
+		createService(historyModel, newHistory);
+		return res.status(200).json({ data: newHistory });
+	} catch (error: any) {
+		console.log(error);
+		return res.status(500).json({ error: ERROR_RESPONSE[500] });
+	}
 };
 
 export { buyItem, sellItem };
