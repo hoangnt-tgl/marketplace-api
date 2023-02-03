c4522e21d85498f851d3930353f3b7e2cfd390af
space-be <space-be@Admins-iMac.local>
2023-02-03 14:58:07 +0700

    commit

--- src/middlewares/checkUser.middlewares.ts
+++ src/middlewares/checkUser.middlewares.ts
@@ -5,10 +5,7 @@ import { ERROR_RESPONSE } from "../constant/response.constants";
 
 export const checkUserExist = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		let userAddress = 
-			req.body.userAddress || 
-			req.params.userAddress || 
-			req.query.userAddress;
+		let userAddress = req.body.userAddress || req.params.userAddress || req.query.userAddress;
 		userAddress = userAddress.toLowerCase();
 		if (!userAddress) {
 			return res.status(400).json({ error: "Not found UserAddress" });
@@ -26,12 +23,9 @@ export const checkUserExist = async (req: Request, res: Response, next: NextFunc
 export const checkUserAddressValid = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		let { userAddress } = req.body;
-		if (userAddress.length !== 66) 
-		return res.status(400).json({ error: ERROR_RESPONSE[400] });
-		next(); 
+		if (userAddress.length !== 66) return res.status(400).json({ error: ERROR_RESPONSE[400] });
+		next();
 	} catch (error: any) {
 		return res.status(500).json({ error: ERROR_RESPONSE[500] });
 	}
-}
-
-
+};
